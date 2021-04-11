ef666d8bbdc44a8b30ef8b1ab8f66aff1289c7be
nubenum <github@nubenum.de>
2021-04-11 21:08:03 +0000

    Added Parameters -> CO2 -> Temperature -> Sealevel mappings, display params and temperature in UI

--- controls.js
+++ controls.js
@@ -1,18 +1,92 @@
 const SEA_COLOR = '#001234';
 
-const coalplants = document.getElementById('coalplants');
-
 var yearSlider = $('#timeline').slider().on('change', updatePolygons).data('slider');
 
-coalplants.oninput = function() {
+let mappings = undefined;
+
+function createParameters(json) {
+    mappings = json;
+
+    const controls = document.getElementById('parameters');
+    const params = mappings.parameters.mapping
+    for (let i=0; i<params.length; i++) {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = params[i].id;
+        checkbox.id = params[i].id;
+        checkbox.oninput = updatePolygons;
+
+        const label = document.createElement('label');
+        label.htmlFor = params[i].id;
+        label.innerHTML = params[i].label;
+
+        const div = document.createElement('div');
+        div.appendChild(checkbox);
+        div.appendChild(label);
+        controls.appendChild(div);
+    }
     updatePolygons();
 }
 
 function updatePolygons() {
-    const year = yearSlider.getValue();
-    map.setPaintProperty('sealevel_100', 'fill-opacity', year <= 2020 || coalplants.checked ? 1 : 0);
-    map.setPaintProperty('sealevel_1', 'fill-opacity', year > 2020 && year <= 2040 && !coalplants.checked ? 1 : 0);
-    map.setPaintProperty('sealevel_2', 'fill-opacity', year > 2040 && !coalplants.checked ? 1 : 0);
+    const emissions = cumulateCo2Emissions();
+    console.log('Cumulated CO2 emissions until 2100:', emissions);
+    const temperature = interpolate(mappings.co2.mapping, emissions);
+    console.log('Temperature forecast for 2100:', temperature);
+    const sealevel = interpolate(mappings.sealevel.mapping, temperature);  
+    console.log('Sealevel forecast for 2100:', sealevel);
+
+    updateTemperature(temperature);
+    updateSealevel(sealevel);
+}
+
+function cumulateCo2Emissions() {
+    let cumul_co2 = mappings.baseline.mapping[0].y;
+    const params = mappings.parameters.mapping
+    for (let i=0; i<params.length; i++) {
+        const checkbox = document.getElementById(params[i].id);
+        if (checkbox.checked) {
+            cumul_co2 += params[i].y;
+        }
+    }
+    return cumul_co2;
+}
+
+function interpolate(mapping, x) {
+    let upper = undefined;
+    let lower = undefined;
+    for (let i=0; i<mapping.length; i++) {
+        if (mapping[i].x >= x && (upper == undefined || mapping[i].x < upper.x)) {
+            upper = mapping[i];
+        }
+        if (mapping[i].x <= x && (lower == undefined || mapping[i].x > lower.x)) {
+            lower = mapping[i];
+        }
+    }
+    if (upper == lower)
+        return upper.y;
+    if (lower == undefined)
+        return upper.y;
+    if (upper == undefined)
+        return lower.y;
+    return (x-lower.x)/(upper.x-lower.x)*(upper.y-lower.y)+lower.y
+}
+
+function updateTemperature(temperature) {
+    document.getElementById('temperature').innerHTML = Math.round((temperature+1)*10)/10;
+}
+function updateSealevel(sealevel) {
+    const layers = ['100', '05', '1', '2', '3'];
+    const levels = [-100, 0.2, 0.7, 1.5, 2.5, 100];
+    for (let i=0; i<layers.length; i++) {
+        map.setPaintProperty('sealevel_' + layers[i], 'fill-opacity', sealevel >= levels[i] && sealevel < levels[i+1]  ? 0.9 : 0);  
+    }
+}
+
+function loadMappings() {
+    fetch('data/mappings.json')
+    .then(response => response.json())
+    .then(json => createParameters(json)); 
 }
 
 function loadFile(id, filename) {
@@ -70,13 +144,14 @@ map.on('load', function () {
     });
 
     addTileLayerToMap('sealevel_100');
+    addTileLayerToMap('sealevel_05');
     addTileLayerToMap('sealevel_1');
     addTileLayerToMap('sealevel_2');
+    addTileLayerToMap('sealevel_3');
     
-    updatePolygons();
+    loadMappings();
     
     //loadFile("sealevel_-100", "data/sealevel/sealevel_-15.geojson");
     //loadFile("sealevel_1", "data/sealevel/sealevel_1.geojson");
     //loadFile("sealevel_2", "data/sealevel/sealevel_2.geojson");
-    //loadFile("poles", "data/sealevel/poles.geojson");
 });
